1eb3d78657508ad924195a6a61fb978a8a0bab93
digika-ding <weibing.ding@digika.jp>
2022-11-28 00:53:08 +0900

    test

---
+++ src/main/java/com/digika/jwt/controller/PlayerController.java
@@ -0,0 +1,15 @@
+package com.digika.jwt.controller;
+
+import com.digika.jwt.dto.Player;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/players")
+public class PlayerController {
+    @GetMapping("list")
+    public List<Player> listPlayers() {
+        return List.of(new Player("0123456789", "Bob"), new Player("9876543210", "Alice"));
+    }
+}
---
+++ src/main/java/com/digika/jwt/dto/Player.java
@@ -0,0 +1,4 @@
+package com.digika.jwt.dto;
+
+public record Player(String id, String nickname) {
+}
